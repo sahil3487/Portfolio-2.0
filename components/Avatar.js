fdf18b6b3de23c4e068e0c84a852ed61f5dfa7b4
Sahil Singh Baghel <102261017+sahil3487@users.noreply.github.com>
2024-06-26 16:25:01 +0530

    3

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import avatarImage from "../public/avatar.png"; // Make sure this path is correct
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className=" hidden xl:flex xl:max-w-none justify-center items-center mr-[250px] ">
+      <Image
+        src={avatarImage}
+        width={450} // Adjusted width as per your modified dimensions
+        height={380} // Adjusted height as per your modified dimensions
+        alt="Avatar Image"
+        className="translate-z-0"
+      />
+    </div>
+  );
 };
 
 export default Avatar;
