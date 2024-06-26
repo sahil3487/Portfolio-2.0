c2319e9e11abcfb99be4d5e9c6177d4db9e3c54d
Sahil Singh Baghel <102261017+sahil3487@users.noreply.github.com>
2024-06-26 16:11:13 +0530

    secound

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,35 @@
+// Next image
+
+import Image from "next/image";
+
+//next Link
+import Link from "next/link";
+//components
+
+import Socials from "../components/Socials";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className=" absolute z-30 w-full flex items-center px-16 xl:px0 xl:h-[90px]">
+      <div className=" container mx-auto">
+        <div className=" flex flex-col lg:flex-row justify-between items-center gap-y-7 py-8">
+          {/* logo */}
+          <Link href={"/"}>
+            <Image
+              src={"/logo.svg"}
+              width={220}
+              height={48}
+              alt=""
+              priority={true}
+            />
+          </Link>
+        </div>
+
+        {/* socials */}
+        <Socials />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,13 @@
+import Image from "next/image"
+import im  from "../public/top-left-img.png";
+
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+  return <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[2000px] xl:w-[400px]
+  opacity-50">
+
+    <Image src= {im} widtth = {400} height={400} />
+    
+  </div>;
 };
 
 export default TopLeftImg;
