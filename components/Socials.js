54ca426fd41beed0551041759c4a840f405fe176
Sahil Singh Baghel <102261017+sahil3487@users.noreply.github.com>
2024-07-08 02:15:41 +0530

    finalpush

--- components/Socials.js
+++ components/Socials.js
@@ -4,12 +4,13 @@ import {
   RiInstagramLine,
   RiFacebookBoxLine,
   RiLinkedinBoxLine,
-  RiTwitterLine
+  RiTwitterLine,
+  RiGithubLine
 } from "react-icons/ri";
 
 const Socials = () => {
   return (
-    <div className="flex items-center justify-center lg:justify-start gap-x-8 text-lg lg:pl-10  ">
+    <div className="flex items-center justify-center lg:justify-start gap-x-5 text-lg lg:pl-10  ">
       <Link href="https://www.youtube.com/@mr.automotive3487" className="hover:text-accent transition-all duration-300 transform hover:scale-110">
         <RiYoutubeLine />
       </Link>
@@ -24,6 +25,9 @@ const Socials = () => {
       </Link>
       <Link href="https://www.linkedin.com/in/sahil-singh-baghel/" className="hover:text-accent transition-all duration-300 transform hover:scale-110">
         <RiLinkedinBoxLine />
+      </Link>
+      <Link href="https://github.com/sahil3487" className="hover:text-accent transition-all duration-300 transform hover:scale-110">
+        <RiGithubLine />
       </Link>
     </div>
   );
